4f1105c96c1c8c44a0588320ad2afb3f59d077b4
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-09 19:52:22 +0400

    feat: create constant for projects description

---
+++ script/constants.js
@@ -0,0 +1,18 @@
+export const projects_list = [
+  {
+    image: "project-cover-0.jpg",
+    title: "Project title",
+    paragraph: "some text some text some text",
+    stack: ["HTML", "JavaScript"],
+    live_url: "",
+    source_code: "",
+  },
+  {
+    image: "project-cover-0.jpg",
+    title: "Project title",
+    paragraph: "some text some text some text",
+    stack: ["HTML", "JavaScript"],
+    live_url: "",
+    source_code: "",
+  },
+];
--- script/projects.js
+++ script/projects.js
@@ -1,8 +1,10 @@
+import { projects_list as data } from "./constants.js";
 function render_projects() {
   const projects_list = document.getElementById("projects-list");
   const project_card = document.createElement("div");
   project_card.textContent = "project card";
   projects_list.appendChild(project_card);
+  console.log(data);
 }
 
 render_projects();
