2504c5f4ee84e78f4c9be5f9f77e81e5116cd450
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-09 19:37:20 +0400

    feat(projects-list): add first draft conntent using js dom

--- script/projects.js
+++ script/projects.js
@@ -1,5 +1,8 @@
 function render_projects() {
-  console.log("render projects");
+  const projects_list = document.getElementById("projects-list");
+  const project_card = document.createElement("div");
+  project_card.textContent = "project card";
+  projects_list.appendChild(project_card);
 }
 
 render_projects();
