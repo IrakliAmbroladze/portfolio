27e48d6b7b9b47e726b11a98801d09c74c7a3650
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-20 14:48:15 +0400

    style(project-container): create content container div

--- script/components/Project_Card.js
+++ script/components/Project_Card.js
@@ -9,6 +9,7 @@ export const Project_Card = ({
 
   const cover_container = document.createElement("div");
   const cover = document.createElement("img");
+  const project_content_container = document.createElement("div");
   const title = document.createElement("h2");
   const description = document.createElement("p");
   const stack = document.createElement("h3");
@@ -17,19 +18,22 @@ export const Project_Card = ({
 
   cover_container.className = "cover-container";
   cover.className = "cover";
-  cover_container.appendChild(cover);
+  project_content_container.className = "project-content-container";
 
   title.textContent = project.title;
   card_container_element.appendChild(title);
   cover.src = cover_src;
   cover.alt = cover_alt;
-  card_container_element.append(
-    cover_container,
+
+  cover_container.appendChild(cover);
+  project_content_container.append(
     title,
     description,
     stack,
     live_url,
     source_code,
   );
+
+  card_container_element.append(cover_container, project_content_container);
   grid_container_element.appendChild(card_container_element);
 };
