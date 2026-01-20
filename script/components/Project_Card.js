ec970587e3b01730136bcd940bf7f243d3e00a0c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-20 13:24:35 +0400

    feat(Project_Card): add all necessary initial html element

--- script/components/Project_Card.js
+++ script/components/Project_Card.js
@@ -5,12 +5,26 @@ export const Project_Card = ({
   cover_alt,
 }) => {
   const card_container_element = document.createElement("div");
+  card_container_element.className = "project";
+
+  const cover = document.createElement("img");
   const title = document.createElement("h2");
+  const description = document.createElement("p");
+  const stack = document.createElement("h3");
+  const live_url = document.createElement("a");
+  const source_code = document.createElement("a");
+
   title.textContent = project.title;
   card_container_element.appendChild(title);
-  const cover = document.createElement("img");
   cover.src = cover_src;
   cover.alt = cover_alt;
-  card_container_element.appendChild(cover);
+  card_container_element.append(
+    cover,
+    title,
+    description,
+    stack,
+    live_url,
+    source_code,
+  );
   grid_container_element.appendChild(card_container_element);
 };
