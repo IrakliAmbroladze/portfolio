28e6a75d1a893e309134d84278a6cfc884560abf
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-20 12:27:27 +0400

    refac(Projects): pass cover_src and cover_alt as props

--- script/components/Projects.js
+++ script/components/Projects.js
@@ -1,9 +1,14 @@
-import { projects_list } from "../constants.js";
+import { projects_list, PROJECTS_COVERS_DIRECTORY } from "../constants.js";
 import { Project_Card } from "./Project_Card.js";
 
 export function Projects() {
   const projects_list_element = document.getElementById("projects-list");
   projects_list.forEach((project) =>
-    Project_Card(project, projects_list_element),
+    Project_Card({
+      project,
+      grid_container_element: projects_list_element,
+      cover_src: `${PROJECTS_COVERS_DIRECTORY}/${project.image}`,
+      cover_alt: project.image,
+    }),
   );
 }
