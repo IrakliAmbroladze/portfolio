908a458eff4746bad386fbc466dc49f597c8f42e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-20 12:28:10 +0400

    feat(Project_Card): import image dynamically

--- script/components/Project_Card.js
+++ script/components/Project_Card.js
@@ -1,7 +1,16 @@
-export const Project_Card = (project, grid_container_element) => {
+export const Project_Card = ({
+  project,
+  grid_container_element,
+  cover_src,
+  cover_alt,
+}) => {
   const card_container_element = document.createElement("div");
   const title = document.createElement("h2");
   title.textContent = project.title;
   card_container_element.appendChild(title);
+  const cover = document.createElement("img");
+  cover.src = cover_src;
+  cover.alt = cover_alt;
+  card_container_element.appendChild(cover);
   grid_container_element.appendChild(card_container_element);
 };
