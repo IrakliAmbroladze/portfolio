ebb4706f5705d5895ea4d952ddcab8888f71bb8a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-27 23:51:53 +0400

    feat(Articles): used projects styles and funcitons for creating foundation for articles

--- script/components/Articles.js
+++ script/components/Articles.js
@@ -1,10 +1,11 @@
 import { Section } from "../classes/Section.js";
+import { articles_list, PROJECTS_COVERS_DIRECTORY } from "../constants.js";
+import { Project_Card } from "./Project_Card.js";
 
 export function Articles() {
   const section = document.getElementById("articles");
   const articles = new Section("Articles", "Articles I have written so far");
-  section.classList.add("container");
-  section.classList.add("section");
+  section.classList.add("container", "section", "projects");
 
   const title = document.createElement("h2");
   title.className = "section-title";
@@ -12,7 +13,17 @@ export function Articles() {
   const subheader = document.createElement("h2");
   subheader.className = "section-paragraph";
   subheader.innerHTML = articles.subheader;
-  section.append(title, subheader);
-  console.log(articles);
-  console.log(section);
+
+  const articlesList = document.createElement("div");
+  articlesList.className = "projects-list";
+  section.append(title, subheader, articlesList);
+
+  articles_list.forEach((project) =>
+    Project_Card({
+      project,
+      grid_container_element: articlesList,
+      cover_src: `${PROJECTS_COVERS_DIRECTORY}/${project.image}`,
+      cover_alt: project.image,
+    }),
+  );
 }
--- script/constants.js
+++ script/constants.js
@@ -29,3 +29,33 @@ export const projects_list = [
 ];
 
 export const PROJECTS_COVERS_DIRECTORY = "images/project-covers";
+
+export const articles_list = [
+  {
+    image: "chess.png",
+    title: "Chess",
+    paragraph:
+      "A full-stack chess application built entirely in Rust, featuring real-time multiplayer gameplay with WebSockets, timers, and full chess rules validation.",
+    stack: ["Rust", "Leptos", "Axum", "WebSockets"],
+    live_url: "https://chess-app-irakli.fly.dev/",
+    source_code: "https://github.com/IrakliAmbroladze/chess-app",
+  },
+  {
+    image: "primal_training.png",
+    title: "Primal training",
+    paragraph:
+      "A fully responsive landing page built with HTML and SCSS, following a component-based structure and modern UI practices. The project supports desktop, tablet, and mobile views.",
+    stack: ["HTML", "SCSS"],
+    live_url: "https://irakliambroladze.github.io/primal-training/",
+    source_code: "https://github.com/IrakliAmbroladze/primal-training",
+  },
+  {
+    image: "weather_app.jpg",
+    title: "Weather Forecast",
+    paragraph:
+      "A modern, responsive weather application built with React and Vite that provides real-time weather information for cities worldwide.",
+    stack: ["React", "Tailwind CSS"],
+    live_url: "https://amindi-app.vercel.app/",
+    source_code: "https://github.com/IrakliAmbroladze/weather-app",
+  },
+];
